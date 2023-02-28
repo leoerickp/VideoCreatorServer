0106bdbf1318f979fa1ce267b3c1d3077d146ffd
Leo Erick Pereyra Rodriguez <leoerickp@gmail.com>
2023-02-28 18:02:45 -0400

    Swagger documentation corrected

--- src/routes/swagger.routes.ts
+++ src/routes/swagger.routes.ts
@@ -9,7 +9,7 @@ const options: swaggerJSDoc.Options = {
         info: {
             title: ' Video Creator API',
             version: '1.0.0',
-            description: 'Video Creator API manage data for .... This an example created for this project. It was writen using [https://editor.swagger.io/](https://editor.swagger.io/) as editor and **swagger-jsdoc** and **swagger-ui-express** component'
+            description: 'Video Creator API manage data for .... This is an example created for this project. It was written using [https://editor.swagger.io/](https://editor.swagger.io/) as editor and **swagger-jsdoc** and **swagger-ui-express** component'
         }
     },
     apis: ['src/routes/*.routes.ts']
